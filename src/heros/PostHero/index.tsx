d6bb793e58475ebafceeb8a4bbefab1a4d6f8051
moh92ahm <mohamad92ahm@gmail.com>
2025-07-17 13:30:56 +0300

    post page is ready and working fine

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -15,16 +15,14 @@ export const PostHero: React.FC<{
     populatedAuthors && populatedAuthors.length > 0 && formatAuthors(populatedAuthors) !== ''
 
   return (
-    <div className="relative -mt-[10.4rem] flex items-end">
       <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
         <div className="col-start-1 col-span-1 md:col-start-2 md:col-span-2">
+          {/* Categories */}
           <div className="uppercase text-sm mb-6">
             {categories?.map((category, index) => {
               if (typeof category === 'object' && category !== null) {
                 const { title: categoryTitle } = category
-
                 const titleToUse = categoryTitle || 'Untitled category'
-
                 const isLast = index === categories.length - 1
 
                 return (
@@ -38,16 +36,17 @@ export const PostHero: React.FC<{
             })}
           </div>
 
-          <div className="">
-            <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{title}</h1>
+          {/* Title */}
+          <div>
+            <h1 className="mb-2 text-3xl md:text-5xl lg:text-6xl font-heading font-bold">{title}</h1>
           </div>
 
-          <div className="flex flex-col md:flex-row gap-4 md:gap-16">
+          {/* Meta Info */}
+          <div className="flex flex-col md:flex-row gap-4 md:gap-16 mb-6">
             {hasAuthors && (
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-1">
                   <p className="text-sm">Author</p>
-
                   <p>{formatAuthors(populatedAuthors)}</p>
                 </div>
               </div>
@@ -55,19 +54,22 @@ export const PostHero: React.FC<{
             {publishedAt && (
               <div className="flex flex-col gap-1">
                 <p className="text-sm">Date Published</p>
-
                 <time dateTime={publishedAt}>{formatDateTime(publishedAt)}</time>
               </div>
             )}
           </div>
+
+          {/* Feature Image */}
+          {heroImage && typeof heroImage !== 'string' && (
+            <div className="w-full max-w-[48rem] mb-8">
+              <Media
+                resource={heroImage}
+                imgClassName="object-cover w-full rounded"
+                priority
+              />
+            </div>
+          )}
         </div>
       </div>
-      <div className="min-h-[80vh] select-none">
-        {heroImage && typeof heroImage !== 'string' && (
-          <Media fill priority imgClassName="-z-10 object-cover" resource={heroImage} />
-        )}
-        <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
-      </div>
-    </div>
   )
 }
