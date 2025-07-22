0ac6f4a87dced4517014bd1fc462c1b816dfb033
moh92ahm <mohamad92ahm@gmail.com>
2025-07-22 15:45:56 +0300

    favicon handled

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -45,8 +45,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     >
       <head>
         <InitTheme />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/Favicon.png" rel="icon" sizes="32x32" />
+        <link href="/Favicon.png" rel="icon" type="image/svg+xml" />
       </head>
       <body className='bg-background text-foreground antialiased min-h-screen flex flex-col'>
         <Providers>
