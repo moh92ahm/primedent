6676aa124a3284054e6f28a967105348c96f7b16
moh92ahm <mohamad92ahm@gmail.com>
2025-07-24 09:28:02 +0300

    migration added

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250723_124350 from './20250723_124350';
+import * as migration_20250723_142117 from './20250723_142117';
 
 export const migrations = [
   {
     up: migration_20250723_124350.up,
     down: migration_20250723_124350.down,
-    name: '20250723_124350'
+    name: '20250723_124350',
+  },
+  {
+    up: migration_20250723_142117.up,
+    down: migration_20250723_142117.down,
+    name: '20250723_142117'
   },
 ];
